5731bcd3e74577813f82b6d65e8673e50d9d5985
Nate Cottle <cottle.nate@gmail.com>
2018-06-27 10:52:27 -0700

    add passing tests for ties and losses.

--- lib/rps.rb
+++ lib/rps.rb
@@ -4,10 +4,12 @@ class RPS
   # end
 
   def wins? (player, input)
-    if player == 'rock' && input == 'scissors'
+    if (player == 'rock' && input == 'scissors') || (player == 'scissors' && input == 'paper') || (player == 'paper' && input == 'rock')
       true
-    elsif player == 'scissors' && input == 'paper'
-      true
+    elsif (player == 'paper' && input == 'paper') || (player == 'rock' && input == 'rock') || (player == 'scissors' && input == 'scissors')
+      false
+    elsif (player == 'scissors' && input == 'rock') || (player == 'paper' && input == 'scissors') || (player == 'rock' && input == 'paper')
+      false
     end
   end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -11,4 +11,19 @@ describe('rps') do
     game = RPS.new()
     expect(game.wins?("scissors", "paper")).to(eq(true))
   end
+
+  it("returns true if paper is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins?("paper", "rock")).to(eq(true))
+  end
+
+  it("returns false if object and argument are the same") do
+    game = RPS.new()
+    expect(game.wins?("paper", "paper")).to(eq(false))
+  end
+
+  it("returns false if scissors is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "rock")).to(eq(false))
+  end
 end
